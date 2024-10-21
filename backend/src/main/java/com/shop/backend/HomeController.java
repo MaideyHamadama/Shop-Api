31abf893403037453d4cbaa811925e6b98438a24
IVANSTEVE <kuatses@gmail.com>
2024-10-22 00:33:10 +0200

    Initialisation de la partie BackEnd

---
+++ backend/src/main/java/com/shop/backend/HomeController.java
@@ -0,0 +1,13 @@
+package com.shop.backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Bienvenue sur le backend de vente de t-shirt!";
+    }
+}
