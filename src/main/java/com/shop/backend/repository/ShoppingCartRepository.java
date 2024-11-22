609aa82ec3c341a3787ef12cb5b16aa1ca35b458
aydenmrt <pro@arpm.dev>
2024-11-22 06:22:19 +0100

    Suppression de findById repo ShoppingCart

--- src/main/java/com/shop/backend/repository/ShoppingCartRepository.java
+++ src/main/java/com/shop/backend/repository/ShoppingCartRepository.java
@@ -1,4 +1,4 @@
-/*package com.shop.backend.repository;
+package com.shop.backend.repository;
 
 import com.shop.backend.entity.ShoppingCart;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,11 +7,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Integer> {
-
-    // Trouver un panier par son ID
-    ShoppingCart findById(int cartID);
-
     // Trouver un panier par l'ID de l'utilisateur
     ShoppingCart findByUserUserID(int userID);
 }
-*/
