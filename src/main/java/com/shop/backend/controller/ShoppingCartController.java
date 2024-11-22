778833caeb622e2c03110994974788efe4dc7869
aydenmrt <pro@arpm.dev>
2024-11-22 06:57:48 +0100

    Correction variables suite a non utilisation des ProductVariants

--- src/main/java/com/shop/backend/controller/ShoppingCartController.java
+++ src/main/java/com/shop/backend/controller/ShoppingCartController.java
@@ -1,79 +1,91 @@
-/*package com.shop.backend.controller;
- 
+package com.shop.backend.controller;
+
 import com.shop.backend.entity.ShoppingCart;
 import com.shop.backend.entity.ShoppingCartLine;
 import com.shop.backend.entity.Product;
 import com.shop.backend.repository.ShoppingCartRepository;
 import com.shop.backend.repository.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.shop.backend.repository.UserRepository;
+import com.shop.backend.service.ShoppingCartService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.yaml.snakeyaml.events.Event.ID;
- 
+
 import java.util.Optional;
- 
+
 @RestController
 @RequestMapping("/cart")
 public class ShoppingCartController {
- 
-    @Autowired
-    private ShoppingCartRepository shoppingCartRepository;
- 
-    @Autowired
-    private ProductRepository productRepository;
- 
+
+
+    private final ShoppingCartService shoppingCartService;
+
+    private final ShoppingCartRepository shoppingCartRepository;
+
+    private final ProductRepository productRepository;
+
+    private final UserRepository userRepository;
+
+    public ShoppingCartController(ShoppingCartService shoppingCartService, ShoppingCartRepository shoppingCartRepository,
+                                  ProductRepository productRepository, UserRepository userRepository) {
+        this.shoppingCartService = shoppingCartService;
+        this.shoppingCartRepository = shoppingCartRepository;
+        this.productRepository = productRepository;
+        this.userRepository = userRepository;
+    }
+
     /**
      * Ajouter un produit au panier
-     * @param cartId L'ID du panier
+     *
+     * @param cartId    L'ID du panier
      * @param productId L'ID du produit à ajouter
-     * @param quantity La quantité du produit à ajouter
+     * @param quantity  La quantité du produit à ajouter
      * @return Le panier mis à jour
      */
-   /*  @PostMapping("/{cartId}/add")
-    public ResponseEntity<ShoppingCart> addToCart(@PathVariable int cartId, @RequestParam int idProduct, @RequestParam int quantity) {
+    @PostMapping("/{cartId}/add")
+    public ResponseEntity<ShoppingCart> addToCart(@PathVariable int cartId, @RequestParam int productId, @RequestParam int quantity) {
         // Récupérer le panier
         Optional<ShoppingCart> shoppingCartOptional = shoppingCartRepository.findById(cartId);
         if (!shoppingCartOptional.isPresent()) {
             return ResponseEntity.notFound().build();
         }
         ShoppingCart cart = shoppingCartOptional.get();
- 
+
         // Récupérer le produit
-        Optional<Product> productOptional = productRepository.findById(idProduct) ;
+        Optional<Product> productOptional = productRepository.findById(productId);
         if (!productOptional.isPresent()) {
             return ResponseEntity.notFound().build();
         }
         Product product = productOptional.get();
- 
+
         // Vérifier si le produit existe déjà dans le panier
         ShoppingCartLine existingLine = cart.getCartProducts().stream()
-                .filter(line -> line.getProduct().getId().equals(idProduct))
+                .filter(line -> line.getProduct().getIdProduct() == productId)
                 .findFirst()
                 .orElse(null);
- 
+
         if (existingLine != null) {
             // Si le produit est déjà dans le panier, mettre à jour la quantité
             existingLine.setQuantity(existingLine.getQuantity() + quantity);
         } else {
             // Sinon, ajouter un nouvel article dans le panier
-            ShoppingCartLine newLine = new ShoppingCartLine(product, quantity, shoppingCart );
+            ShoppingCartLine newLine = new ShoppingCartLine(product, quantity, cart);
             newLine.setShoppingCart(cart); // Set the shopping cart for the new line
             cart.getCartProducts().add(newLine); // Add the line to the cart
         }
- 
+
         // Calculer le montant total du panier
         double totalAmount = cart.getCartProducts().stream()
                 .mapToDouble(line -> line.getTotalPrice())
                 .sum();
         cart.setCartTotalPrice(totalAmount);
- 
+
         // Sauvegarder les modifications dans la base de données
         shoppingCartRepository.save(cart);
- 
+
         // Retourner le panier mis à jour
         return ResponseEntity.ok(cart);
-    } 
-    @PostMapping("/{cartId}/update")
+    }
+    @PutMapping("/{cartId}/update")
     public ResponseEntity<ShoppingCart> updateQuantity(@PathVariable int cartId, @RequestParam int productId, @RequestParam int quantity) {
         // Récupérer le panier
         ShoppingCart cart = shoppingCartRepository.findById(cartId)
@@ -85,7 +97,7 @@ public class ShoppingCartController {
 
         // Vérifier si le produit existe dans le panier
         ShoppingCartLine existingLine = cart.getCartProducts().stream()
-                .filter(line -> line.getProduct().getId().equals(productId))
+                .filter(line -> line.getProduct().getIdProduct() == productId)
                 .findFirst()
                 .orElse(null);
 
@@ -121,7 +133,7 @@ public class ShoppingCartController {
      * @param productId L'ID du produit à retirer
      * @return Le panier mis à jour
      */
-    /*@PostMapping("/{cartId}/remove")
+    @DeleteMapping("/{cartId}/remove")
     public ResponseEntity<ShoppingCart> removeFromCart(@PathVariable int cartId, @RequestParam int productId) {
         // Récupérer le panier
         ShoppingCart cart = shoppingCartRepository.findById(cartId)
@@ -133,7 +145,7 @@ public class ShoppingCartController {
 
         // Vérifier si le produit existe dans le panier
         ShoppingCartLine lineToRemove = cart.getCartProducts().stream()
-                .filter(line -> line.getProduct().getId().equals(productId))
+                .filter(line -> line.getProduct().getIdProduct() == productId)
                 .findFirst()
                 .orElse(null);
 
@@ -156,14 +168,14 @@ public class ShoppingCartController {
 
         // Retourner le panier mis à jour
         return ResponseEntity.ok(cart);
-    
+
     }
-     /**
+    /**
      * Récupérer le panier d'un utilisateur
      * @param cartId L'ID du panier
      * @return Le panier avec ses lignes de produits
      */
-    /*@GetMapping("/{cartId}")
+    @GetMapping("/{cartId}")
     public ResponseEntity<ShoppingCart> getCart(@PathVariable int cartId) {
         // Récupérer le panier
         ShoppingCart cart = shoppingCartRepository.findById(cartId)
@@ -173,4 +185,3 @@ public class ShoppingCartController {
         return ResponseEntity.ok(cart);
     }
 }
-*/
