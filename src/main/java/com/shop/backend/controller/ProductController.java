f86cb9e0d363368a36394f836bac14b9500695a5
aydenmrt <pro@arpm.dev>
2024-11-29 11:44:34 +0100

    Refactoring : Déplacement et découpage des méthodes dans les services

--- src/main/java/com/shop/backend/controller/ProductController.java
+++ src/main/java/com/shop/backend/controller/ProductController.java
@@ -1,7 +1,9 @@
 package com.shop.backend.controller;
 
 import com.shop.backend.entity.Product;
+import com.shop.backend.dto.ProductDTO;
 import com.shop.backend.repository.ProductRepository;
+import com.shop.backend.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,26 +19,29 @@ import java.util.List;
 public class ProductController {
 
     @Autowired
-    private ProductRepository ProductRepository;
+    private ProductRepository productRepository;
+
+    @Autowired
+    private ProductService productService;
 
     /**
      * Retrieves the list of all products from the database.
      *
      * @return A {@link ResponseEntity} containing a list of all {@link Product} instances.
      *
-     *         If the retrieval is successful, it returns an HTTP 200 (OK) status
-     *         with the products in the response body.
-     *         If no products are found, it returns an HTTP 204 (No Content) status.
+     * If the retrieval is successful, it returns an HTTP 200 (OK) status
+     * with the products in the response body.
+     * If no products are found, it returns an HTTP 204 (No Content) status.
      *
      * This method allows the client to retrieve all products from the database for display purposes.
      */
     @GetMapping
-    public ResponseEntity<List<Product>> getAllProducts() {
-        List<Product> products = ProductRepository.findAll();
-        if (products.isEmpty()) {
+    public ResponseEntity<List<ProductDTO>> getAllProducts() {
+        List<ProductDTO> productDTOs = productService.getAllProducts();
+        if (productDTOs.isEmpty()) {
             return ResponseEntity.noContent().build();
         }
-        return ResponseEntity.ok().body(products);
+        return ResponseEntity.ok().body(productDTOs);
     }
 
     /**
@@ -45,18 +50,19 @@ public class ProductController {
      * @param id The ID of the product to retrieve.
      * @return A {@link ResponseEntity} containing the information of the {@link Product}.
      *
-     *         If the retrieval is successful, it returns an HTTP 200 (OK) status
-     *         with the product details in the response body.
-     *         If the product does not exist, it returns an HTTP 404 (Not Found) status.
+     * If the retrieval is successful, it returns an HTTP 200 (OK) status
+     * with the product details in the response body.
+     * If the product does not exist, it returns an HTTP 404 (Not Found) status.
      *
      * This method allows the client to retrieve the details of a product for display purposes.
      */
     @GetMapping("/{id}")
-    public ResponseEntity<Product> getProductById(@PathVariable int id) {
-        Product product = ProductRepository.findById(id).orElse(null);
-        if (product == null) {
+    public ResponseEntity<ProductDTO> getProductById(@PathVariable int id) {
+        try {
+            ProductDTO productDTO = productService.getProductById(id);
+            return ResponseEntity.ok().body(productDTO);
+        } catch (RuntimeException e) {
             return ResponseEntity.notFound().build();
         }
-        return ResponseEntity.ok().body(product);
     }
 }
--- src/main/java/com/shop/backend/service/ShoppingCartService.java
+++ src/main/java/com/shop/backend/service/ShoppingCartService.java
@@ -1,36 +1,129 @@
 package com.shop.backend.service;
 
+import com.shop.backend.dto.ShoppingCartDTO;
 import com.shop.backend.entity.Product;
 import com.shop.backend.entity.ShoppingCart;
 import com.shop.backend.entity.ShoppingCartLine;
 import com.shop.backend.entity.User;
+import com.shop.backend.repository.ProductRepository;
+import com.shop.backend.repository.ShoppingCartLineRepository;
 import com.shop.backend.repository.ShoppingCartRepository;
+import com.shop.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
 
 @Service
 public class ShoppingCartService {
 
-    private final ShoppingCartRepository shoppingCartRepository;
+    @Autowired
+    private ShoppingCartRepository shoppingCartRepository;
 
-    public ShoppingCartService(ShoppingCartRepository shoppingCartRepository) {
-        this.shoppingCartRepository = shoppingCartRepository;
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private ShoppingCartLineRepository shoppingCartLineRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public ShoppingCart getShoppingCartById(int cartId) {
+        return shoppingCartRepository.findById(cartId)
+                .orElseThrow(() -> new RuntimeException("Panier non trouvé avec l'ID: " + cartId));
     }
 
-    public ShoppingCart createCart(User user) {
-        ShoppingCart cart;
-        if (user == null) {
-            cart = new ShoppingCart();
+    private Product getProductById(int productId) {
+        return productRepository.findById(productId)
+                .orElseThrow(() -> new RuntimeException("Produit non trouvé avec l'ID: " + productId));
+    }
+
+    private void reCalculateTotalPrice(ShoppingCart cart) {
+        cart.setCartTotalPrice(cart.getCartProducts().stream()
+                .mapToDouble(ShoppingCartLine::getTotalPrice)
+                .sum());
+    }
+
+    private ShoppingCartLine findProductInCart(ShoppingCart cart, int productId) {
+        return cart.getCartProducts().stream()
+                .filter(line -> line.getProduct().getIdProduct() == productId)
+                .findFirst()
+                .orElse(null);
+    }
+
+    private ShoppingCartLine findOrCreateCartLine(ShoppingCart cart, Product product, int quantity) {
+        ShoppingCartLine line = findProductInCart(cart, product.getIdProduct());
+        if (line == null) {
+            line = new ShoppingCartLine(product, quantity, cart);
+            cart.getCartProducts().add(line);
+        } else {
+            line.setQuantity(line.getQuantity() + quantity);
         }
-        else {
-            cart = new ShoppingCart(user);
+        return line;
+    }
+
+    private ShoppingCartDTO createCartForUser(int userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("Utilisateur non trouvé avec l'ID : " + userId));
+        ShoppingCart cart = new ShoppingCart(user);
+        return new ShoppingCartDTO(shoppingCartRepository.save(cart));
+    }
+
+    public ShoppingCartDTO getOrCreateCartForUser(int userId) {
+        Optional<ShoppingCart> optionalCart = shoppingCartRepository.findByUserId(userId);
+        if (optionalCart.isPresent()) {
+            ShoppingCart cart = optionalCart.get();
+            return new ShoppingCartDTO(cart); // Convertir en DTO
+        } else {
+            return createCartForUser(userId); // Sinon, créer un panier
         }
-        return shoppingCartRepository.save(cart);
     }
 
-    public void addProductToCart(Product product, int quantity) {
+    // Création d'un panier pour un visiteur
+    public ShoppingCartDTO createCartForVisitor() {
+        ShoppingCart cart = new ShoppingCart();
+        shoppingCartRepository.save(cart);
+        return new ShoppingCartDTO(cart);
     }
 
-    public void removeProductFromCart(Product product) {
+    public ShoppingCartDTO addProductToCart(int cartId, int productId, int quantity) {
+        ShoppingCart cart = getShoppingCartById(cartId);
+        Product product = getProductById(productId);
+        findOrCreateCartLine(cart, product, quantity);
+        reCalculateTotalPrice(cart);
+        shoppingCartRepository.save(cart);
+        return new ShoppingCartDTO(cart);
+    }
+
+    public ShoppingCartDTO updateProductQuantity(int cartId, int productId, int quantity) {
+        ShoppingCart cart = getShoppingCartById(cartId);
+        ShoppingCartLine existingLine = findProductInCart(cart, productId);
+        if (existingLine == null) {
+            throw new RuntimeException("Produit non trouvé dans le panier avec l'ID : " + productId);
+        }
+        if (quantity == 0) {
+            cart.getCartProducts().remove(existingLine);
+            shoppingCartLineRepository.delete(existingLine);
+        } else {
+            existingLine.setQuantity(quantity);
+        }
+        reCalculateTotalPrice(cart);
+        shoppingCartRepository.save(cart);
+        return new ShoppingCartDTO(cart);
+    }
+
+    public ShoppingCartDTO removeProductFromCart(int cartId, int productId) {
+        ShoppingCart cart = getShoppingCartById(cartId);
+        ShoppingCartLine lineToRemove = findProductInCart(cart, productId);
+        if (lineToRemove == null) {
+            throw new RuntimeException("Produit non trouvé dans le panier avec l'ID : " + productId);
+        }
+        cart.getCartProducts().remove(lineToRemove);
+        shoppingCartLineRepository.delete(lineToRemove);
+        reCalculateTotalPrice(cart);
+        shoppingCartRepository.save(cart);
+        return new ShoppingCartDTO(cart);
     }
 }
