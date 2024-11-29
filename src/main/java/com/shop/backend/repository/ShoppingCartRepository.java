ce066486f91dcf4096d57f4dd775c635b86d8f3a
aydenmrt <pro@arpm.dev>
2024-11-29 11:45:22 +0100

    Ajout d'un query personnalisé en attente du mappage User/ShoppingCart

--- src/main/java/com/shop/backend/repository/ShoppingCartRepository.java
+++ src/main/java/com/shop/backend/repository/ShoppingCartRepository.java
@@ -2,11 +2,17 @@ package com.shop.backend.repository;
 
 import com.shop.backend.entity.ShoppingCart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 
 @Repository
 public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Integer> {
-    // Trouver un panier par l'ID de l'utilisateur
-    ShoppingCart findByUserUserID(int userID);
+    // Rechercher un panier associé à un utilisateur par son ID
+    // TODO : Vérifier et adapter cette méthode une fois que le mappage entre User et ShoppingCart est validé
+    @Query("SELECT cartID FROM ShoppingCart WHERE user = ?1")
+    Optional<ShoppingCart> findByUserId(@Param("userId") int userId);
 }
