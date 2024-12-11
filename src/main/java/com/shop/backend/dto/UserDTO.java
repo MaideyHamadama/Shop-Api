7ac751390a28cc6d0473368ec452c4ff572a8260
AydRPM <pro@arpm.dev>
2024-12-11 09:21:50 +0100

    Correction gestion panier user (register/login)
    - Merge du cookie cart avec cart en db
    
    - Creation du UserDTO

---
+++ src/main/java/com/shop/backend/dto/UserDTO.java
@@ -0,0 +1,100 @@
+package com.shop.backend.dto;
+
+import com.shop.backend.entity.User;
+
+public class UserDTO {
+    private String email;
+    private String userName;
+    private String userSurname;
+    private String address;
+    private String city;
+    private int codePostal;
+    private Integer cartId;
+    private int numberOfProductsInCart;
+
+    // ============================
+    // Constructeurs
+    // ============================
+
+    public UserDTO() {
+    }
+
+    public UserDTO(User user) {
+        this.email = user.getEmail();
+        this.userName = user.getUserName();
+        this.userSurname = user.getUserSurname();
+        this.address = user.getAddress();
+        this.city = user.getCity();
+        this.codePostal = user.getCodePostal();
+        this.cartId = user.getShoppingCart() != null ? user.getShoppingCart().getCartId() : null;
+        this.numberOfProductsInCart = user.getShoppingCart() != null ? user.getShoppingCart().getCartProducts().size() : 0;
+    }
+
+    // ============================
+    // Getters & Setters
+    // ============================
+
+    public Integer getCartId() {
+        return cartId;
+    }
+
+    public void setCartId(Integer cartId) {
+        this.cartId = cartId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserSurname() {
+        return userSurname;
+    }
+
+    public void setUserSurname(String userSurname) {
+        this.userSurname = userSurname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getCodePostal() {
+        return codePostal;
+    }
+
+    public void setCodePostal(int codePostal) {
+        this.codePostal = codePostal;
+    }
+
+    public int getNumberOfProductsInCart() {
+        return numberOfProductsInCart;
+    }
+
+    public void setNumberOfProductsInCart(int numberOfProductsInCart) {
+        this.numberOfProductsInCart = numberOfProductsInCart;
+    }
+}
--- src/main/java/com/shop/backend/service/AuthService.java
+++ src/main/java/com/shop/backend/service/AuthService.java
@@ -1,16 +1,20 @@
 package com.shop.backend.service;
 
 import java.security.Key;
-import java.util.Base64;
 import java.util.Date;
 
+import com.shop.backend.entity.ShoppingCart;
+import com.shop.backend.entity.ShoppingCartLine;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.security.Keys;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.shop.backend.entity.User;
 import com.shop.backend.repository.UserRepository;
+import com.shop.backend.repository.ShoppingCartRepository;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -21,28 +25,122 @@ public class AuthService {
     @Autowired
     private UserRepository userRepository;
 
-    private final Key secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+    @Autowired
+    private ShoppingCartRepository shoppingCartRepository;
 
+    private final Key secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+    boolean productFound = false;
+
+    // Inscription
     public User register(User user) throws Exception {
+
+        // Vérifier si l'email est déjà utilisé
         if (userRepository.findByEmail(user.getEmail()).isPresent()) {
             throw new Exception("Email déjà utilisé.");
         }
-        // Hacher le mot de passe
+
+        // Hachage du mot de passe
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+        // Assignation du mot de passe haché à l'utilisateur
         user.setPassword(encoder.encode(user.getPassword()));
+
+        // Enregistrement de l'utilisateur dans la db
         return userRepository.save(user);
     }
 
+    // Assigner le panier provenant des cookies à l'utilisateur
+    @Transactional
+    public void assignCartIdToUser(String email, String cartId) {
+
+        // Récupérer l'utilisateur par son email
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("Utilisateur non trouve avec l'email : " + email));
+
+        // Récupérer le panier déjà associé à l'utilisateur dans la db (s'il existe)
+        ShoppingCart existingCart = user.getShoppingCart();
+
+        // Récupérer le panier provenant des cookies
+        ShoppingCart cookieCart = shoppingCartRepository.findById(Integer.parseInt(cartId))
+                .orElseThrow(() -> new RuntimeException("Cart not productFound"));
+
+        // Si panier en db et en cookies fusionner les deux paniers
+        if (existingCart != null) {
+            mergeCarts(existingCart, cookieCart);
+        } else {
+            // Si aucun panier n'est associé, utiliser directement le panier des cookies
+            user.setShoppingCart(cookieCart);
+            cookieCart.setUser(user);
+        }
+
+        // Sauvegarder l'utilisateur avec le panier mis à jour
+        userRepository.save(user);
+
+        // Supprimer l'ancien panier des cookies (si nécessaire)
+//        if (existingCart != null) {
+//            System.out.println("Suppression de l'ancien panier des cookies avec ID: " + cookieCart.getCartId());
+//            shoppingCartRepository.delete(cookieCart);
+//        }
+    }
+
+    private void mergeCarts(ShoppingCart existingCart, ShoppingCart cookieCart) {
+
+        // Parcourir les produits du panier des cookies
+        for (ShoppingCartLine cookieLine : cookieCart.getCartProducts()) {
+            productFound = false;
+
+            // Parcourir les produits du panier existant en db
+            for (ShoppingCartLine existingLine : existingCart.getCartProducts()) {
+                if (existingLine.getProduct().getIdProduct() == cookieLine.getProduct().getIdProduct()) {
+                    // Produit déjà dans le panier : mise à jour de la quantité et recalcul du total
+                    existingLine.setQuantity(existingLine.getQuantity() + cookieLine.getQuantity());
+                    existingLine.setTotalPrice(existingLine.getProduct().getPrice() * existingLine.getQuantity());
+                    productFound = true;
+                    break;
+                }
+            }
+
+            // Produit non trouvé : on ajoute une nouvelle ligne au panier existant
+            if (!productFound) {
+                ShoppingCartLine newLine = new ShoppingCartLine(existingCart, cookieLine.getProduct(), cookieLine.getQuantity());
+                existingCart.getCartProducts().add(newLine);
+            }
+        }
+
+        // Recalculer le total du panier après fusion
+        recalculateCartTotals(existingCart);
+
+        // Sauvegarder les changements
+        shoppingCartRepository.save(existingCart);
+    }
+
+    private void recalculateCartTotals(ShoppingCart cart) {
+        // Faire la somme des prix totaux des produits dans le panier
+        double totalPrice = cart.getCartProducts().stream()
+                .mapToDouble(ShoppingCartLine::getTotalPrice)
+                .sum();
+
+        // Calculer le prix total hors TVA
+        double totalPriceExcludingVAT = totalPrice / (1 + cart.getTvaRate());
+
+        // Mettre à jour les totaux du panier
+        cart.setCartTotalPrice(totalPrice);
+        cart.setCartTotalPriceExcludingVAT(totalPriceExcludingVAT);
+    }
+
+    // Connexion
     public String login(String email, String password) throws Exception {
+        // Récupérer l'utilisateur par son email
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new Exception("Utilisateur non trouvé."));
 
+        // Vérifier le mot de passe
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
         if (!encoder.matches(password, user.getPassword())) {
             throw new Exception("Mot de passe incorrect.");
         }
 
-        // Générer un JWT
+        // Générer un token JWT
         return Jwts.builder()
                 .setSubject(user.getEmail())
                 .setIssuedAt(new Date())
@@ -50,14 +148,30 @@ public class AuthService {
                 .signWith(SignatureAlgorithm.HS512, secretKey)
                 .compact();
     }
+
+    // Récupérer un utilisateur par son email
     public User findByEmail(String email) throws Exception {
         return userRepository.findByEmail(email)
                 .orElseThrow(() -> new Exception("Utilisateur non trouvé."));
     }
 
+    // Récupérer la clé secrète pour le controller
     public Key getSecretKey() {
-        return this.secretKey; // Retourne la clé pour le contrôleur
+        return this.secretKey;
     }
 
-
+    // Méthode de décodage de l'email du token pour pouvoir connecter user directement après l'inscription
+    public String extractEmailFromToken(String token) {
+        try {
+            Claims claims = Jwts.parserBuilder()
+                    .setSigningKey(secretKey)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+            return claims.getSubject();
+        } catch (Exception e) {
+            System.err.println("Erreur lors de la validation du token : " + e.getMessage());
+            return null;
+        }
+    }
 }
